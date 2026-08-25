d455b2b45e90b474c1a6316ec82ae6dfc14a6196
seoiniziosolutions <seo.iniziosolutions@gmail.com>
2026-08-25 14:56:45 +0500

    Deployment Errors
    Co-Authored-By: Khuzaima Shoaib <khuzaima.shoaib@gmail.com>

--- back-end/utils/generateToken.js
+++ back-end/utils/generateToken.js
@@ -8,10 +8,12 @@ export default function generateToken(userId, role) {
 }
 
 export function setTokenCookie(res, token) {
+  const isProduction = process.env.NODE_ENV === "production";
+
   res.cookie("token", token, {
-    httpOnly: true, // JS can't access it
-    secure: process.env.NODE_ENV === "production", // HTTPS only in production
-    sameSite: "strict", // prevents CSRF attacks
-    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
+    httpOnly: true,
+    secure: isProduction,
+    sameSite: isProduction ? "none" : "lax",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 }
