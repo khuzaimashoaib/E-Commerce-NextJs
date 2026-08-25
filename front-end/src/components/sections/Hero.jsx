282b8a10e94b5d2d19ba89bac1b869c360e0d65b
seoiniziosolutions <seo.iniziosolutions@gmail.com>
2026-08-25 15:53:12 +0500

    Updated
    Co-Authored-By: Khuzaima Shoaib <khuzaima.shoaib@gmail.com>

--- front-end/src/components/sections/Hero.jsx
+++ front-end/src/components/sections/Hero.jsx
@@ -4,47 +4,49 @@ import React from "react";
 const Hero = () => {
   return (
     <section className="hero-section-4">
-      <div className="swiper banner-active">
-        <div className="swiper-wrapper">
-          <div className="swiper-slide">
-            <div
-              className="hero-4 bg-cover"
-              style={{
-                backgroundColor: "#96fc80",
-              }}
-            >
-              <div className="container">
-                <div className="row align-items-center">
-                  <div className="col-lg-6">
-                    <div className="hero-content">
-                      <span> NEW ARRIVALS </span>
-                      <h1>
-                        Stunning
-                        <b> Collection </b>
-                      </h1>
-                      <p>Discover Exquisite Pieces for Every Occasion.</p>
-                    </div>
-                    <div className="hero-button">
-                      <Link href="/shop" className="theme-btn">
-                        Explore More
-                      </Link>
-                    </div>
+      <div
+        className="hero-4 bg-cover"
+        style={{
+          backgroundColor: "#96fc80",
+        }}
+      >
+        <div className="container">
+          <div className="row align-items-center">
+            <div className="col-lg-6">
+              <div className="hero-content">
+                <span>NEW ARRIVALS</span>
+
+                <h1>
+                  Stunning <b>Collection</b>
+                </h1>
+
+                <p>Discover Exquisite Pieces for Every Occasion.</p>
+              </div>
+
+              <div className="hero-button">
+                <Link href="/shop" className="theme-btn">
+                  Explore More
+                </Link>
+              </div>
+            </div>
+
+            <div className="col-lg-6">
+              <div className="hero-image">
+                <img
+                  src="/assets/front-end-images/home-img.webp"
+                  alt="Football collection"
+                />
+
+                <div className="icon-box">
+                  <div className="icon">
+                    <img
+                      src="/assets/front-end-images/football-icon.png"
+                      alt="Football"
+                    />
                   </div>
-                  <div className="col-lg-6">
-                    <div className="hero-image">
-                      <img src="assets/front-end-images/home-img.webp" alt="" />
-                      <div className="icon-box ">
-                        <div className="icon">
-                          <img
-                            src="assets/front-end-images/football-icon.png"
-                            alt=""
-                          />
-                        </div>
-                        <div className="discount">
-                          <img src="assets/img/home-4/dis.png" alt="" />
-                        </div>
-                      </div>
-                    </div>
+
+                  <div className="discount">
+                    <img src="/assets/img/home-4/dis.png" alt="Discount" />
                   </div>
                 </div>
               </div>
