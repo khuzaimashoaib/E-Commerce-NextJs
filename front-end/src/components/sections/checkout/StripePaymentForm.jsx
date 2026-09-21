3d157031fea029fba1b76a2cf2d88dfd3b8ec666
seoiniziosolutions <seo.iniziosolutions@gmail.com>
2026-09-21 16:23:04 +0500

    Update env
    Co-Authored-By: Khuzaima Shoaib <khuzaima.shoaib@gmail.com>

--- front-end/src/components/sections/checkout/StripePaymentForm.jsx
+++ front-end/src/components/sections/checkout/StripePaymentForm.jsx
@@ -8,9 +8,7 @@ import {
 import { loadStripe } from "@stripe/stripe-js";
 import { useState } from "react";
 
-const stripePromise = loadStripe(
-  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
-);
+const stripePromise = loadStripe(process.env.NEXT_STRIPE_PUBLISHABLE_KEY);
 
 function StripeForm({ onSuccess, onError }) {
   const result = useCheckoutElements();
