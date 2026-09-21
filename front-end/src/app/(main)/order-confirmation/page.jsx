e889c31bfe42b27b3646c2c64fa945cbb2f2c25e
seoiniziosolutions <seo.iniziosolutions@gmail.com>
2026-09-21 16:34:46 +0500

    Update
    Co-Authored-By: Khuzaima Shoaib <khuzaima.shoaib@gmail.com>

--- front-end/src/app/(main)/order-confirmation/page.jsx
+++ front-end/src/app/(main)/order-confirmation/page.jsx
@@ -1,18 +1,21 @@
 import Breadcrumb from "@/components/sections/Breadcrumb";
 import OrderConfirmation from "@/components/sections/order/OrderConfirmation";
+import { Suspense } from "react";
 
 export default function OrderConfirmationPage() {
   return (
     <>
-      <Breadcrumb
-        title="Order Confirmed"
-        bgImage="/assets/front-end-images/breadcrumb-bg.jpg"
-        items={[
-          { label: "Home", href: "/", icon: "fa-solid fa-house" },
-          { label: "Order Confirmed" },
-        ]}
-      />
-      <OrderConfirmation />
+      <Suspense>
+        <Breadcrumb
+          title="Order Confirmed"
+          bgImage="/assets/front-end-images/breadcrumb-bg.jpg"
+          items={[
+            { label: "Home", href: "/", icon: "fa-solid fa-house" },
+            { label: "Order Confirmed" },
+          ]}
+        />
+        <OrderConfirmation />
+      </Suspense>
     </>
   );
 }
